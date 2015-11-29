291b339f4fa8e423c9a3732318f803d0a4b04128
Suraj Narayanan S <surajx@gmail.com>
2015-11-29 15:31:17 +1100

    Added Question and Choice to admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
