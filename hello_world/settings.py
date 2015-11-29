1b138ceb223016e700631fa9d80389e94e2f096a
Suraj Narayanan S <surajx@gmail.com>
2015-11-29 15:10:35 +1100

    Collected static files

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -104,7 +104,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+STATIC_URL = '/collected_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
